bab472895d0f64d708e47bd99058f65a8bc2c914
DenisAllvz <denilsonideal@gmail.com>
2025-01-24 15:34:49 -0300

    Adicionando de fato a função para coleta de tipo de colunas com dados faltantes

--- src/data_verifications.py
+++ src/data_verifications.py
@@ -17,22 +17,28 @@ y = predict_students_dropout_and_academic_success.data.targets
 #print(X.head())
 #print(x.dtypes)
 
-columns_with_missing_values = x.columns[x.isnull().any()]
 
-#armazena os tipos de dados em colunas que faltam dados
-for column in columns_with_missing_values:
-    column_type = x[column].dtype
-    print(f"'{column}': {column_type}")
+#armazena o tipo das colunas que faltam dados
+def type_column_missing_value(data):
+    columns_with_missing_values = data.columns[data.isnull().any()]
+    column_types = {}  # Dicionário para armazenar os tipos de cada coluna
+    for column in columns_with_missing_values:
+        column_type = data[column].dtype
+        column_types[column] = column_type  # Armazenar tipo por coluna
+        print(f"'{column}': {column_type}")
+    return column_types
+
+print(type_column_missing_value(x))
 
 # Soma todos os dados faltantes por coluna
-missing_data = x.isnull().sum()
+#missing_data = x.isnull().sum()
 
 # Soma todos os dados faltantes em todo dataset
-total_missing = missing_data.sum()
+#total_missing = missing_data.sum()
 
 #Verifica se falta algum dado
-if total_missing == 0:
-    print("Sem dados faltantes")
-else:
-    print(f"Total de dados faltantes: {total_missing}")
-    print(missing_data)
+#if total_missing == 0:
+#    print("Sem dados faltantes")
+#else:
+ #   print(f"Total de dados faltantes: {total_missing}")
+ #   print(missing_data)
